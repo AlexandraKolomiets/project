298054c0731e485dc8e8e12be285faf86b2c36d0
unknown <alexsandrasotnikova@gmail.com>
2019-12-16 14:56:14 +0300

    main

--- main.R
+++ main.R
@@ -1,12 +1,48 @@
-install.packages('httr')
+library(vkR)
 library(httr)
-install.packages('rjson')
 library(rjson)
-ACCESS_TOKEN <- '517eaca4517eaca4517eaca4d55110931f5517e517eaca40ca01e7693ef4566435f380a'
+library(tidyr)
+library(jsonlite)
+library(dplyr)
+library(ggplot2)
+library(lintr)
 
+#Авторизация для получения API key
+vkOAuth(7225265, "photos")
 
-datasets_url <- 'https://api.vk.com/method/database.getCities?country_id=RU&need_all=1&count=1000&access_token=ACCESS_TOKEN&v=V'
-datasets_url <- paste0(datasets_url, ACCESS_TOKEN)
-response <- GET(datasets_url)
+#Полученный ключ нужно вставить ниже
+api_key <- ""
+
+#Создание базы для функционирования тела проекта
+#В первой строке можно добавить другие паблики
+#Код написан для любого количества пабликов
+domains <- c("ru9gag", "fckbrain", "dank_memes_ayylmao")
+publics <- c(1:length(domains))
+publics <- rbind(publics, domains)
+publics <- t(publics)
+colnames(publics)[1] <- ("Number")
+colnames(publics)[2] <- ("Domain")
+table_final <- data.frame(matrix(ncol = 7, nrow = 0))
+colnames(table_final) <- c("public_name", "owner_id", "id_num", "likes_num", "reposts_num", "views_num", "members")
+urls <- data.frame(matrix(ncol = 1, nrow = 0))
+colnames(urls) <- c("Url")
+members <- data.frame(matrix(ncol = 1, nrow = 0))
+colnames(members) <- c("Members")
+
+#Первый запрос к API для получения количества подписчиков по группам
+for (row in 1:nrow(publics)) {
+  adress <- publics[row, 2]
+  ssilka1 <- "https://api.vk.com/method/groups.getById?group_id="
+  ssilka2 <- "&fields=members_count&v=5.103&access_token="
+  ssilka <- as.data.frame(paste0("https://vk.com/", ssilka1, "?w=wall", ssilka2, "_", api_key))
+  api_call_for_members <- paste0(ssilka1, adress, ssilka2, api_key)
+  response_members <- GET(url = api_call_for_members)
+  parsed_con_members <- response_members[6]
+  parsed_members2 <- content(response_members, as = "text")
+  parsed_final_members <- fromJSON(parsed_members2)
+  members_num <- as.data.frame(parsed_final_members$response$members_count)
+  members <- rbind(members, members_num)
+}
+colnames(members) <- c("Members")
 
 
